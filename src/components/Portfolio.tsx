560f5e29d36c890961bfa55d3a1920581e52de8d
MohamedAli1186 <mohameedaallii10@gmail.com>
2025-09-19 22:31:22 +0300

    grid-cols-2

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -113,7 +113,7 @@ const Portfolio = () => {
         </div>
 
         {/* Video Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-8">
           {videos.map((video, index) => (
             <Card
               key={video.id}
